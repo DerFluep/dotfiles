0b0f026948e5d0e1e642fec1542898a11c7711fe
DerFluep <bussejp@gmail.com>
2025-01-04 23:10:09 +0100

    Fix/Change Cursor

--- dot_config/hypr/scripts/executable_screenshot.sh
+++ dot_config/hypr/scripts/executable_screenshot.sh
@@ -4,4 +4,4 @@ title=$(hyprctl -j activewindow | jq -j '"\(.title)"')
 filepath=$HOME/NAS/Photos/Screenshots
 fullpath="${filepath}/${title}"
 
-$HOME/.config/hypr/hyprshot.sh -o $fullpath -m window -m active
+$HOME/.config/hypr/scripts/hyprshot.sh -o $fullpath -m window -m active
